cac4fb2b23797cba3687ce3f2d5bf6119dcdeeb3
Demetrious Robinson <demetriusrobinson.dev@gmail.com>
2019-04-19 20:00:11 -0700

    cred

--- src/utils/userService.js
+++ src/utils/userService.js
@@ -6,18 +6,20 @@ function getUser() {
   return tokenService.getUserFromToken();
 }
 
-function login(creds) {
-  return fetch(BASE_URL + 'login', {
-    method: 'POST',
-	  headers: new Headers({'Content-Type': 'application/json'}),
-    Authorization: "Bearer " + tokenService.getToken(),
-    body: JSON.stringify(creds)
-  })
-  .then(res => {
-    if (res.ok) return res.json();
-    throw new Error('Bad Credentials!');
-  })
-  .then(({token}) => tokenService.setToken(token));
+async function login(creds) {
+	let response = await fetch(BASE_URL + 'login', {
+		method: 'POST',
+		headers: new Headers({ 'Content-Type': 'application/json' }),
+		body: JSON.stringify(creds)
+	}).then(res => res.json())
+		.catch(err => console.log(err));
+	if (response.err) {
+		return response;
+	}
+	else {
+		await tokenService.setToken(response.token);
+		return { status: 'ok' }
+	}
 }
 
 function logout() {
